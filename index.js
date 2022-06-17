0872b135e598fb4f015650966f98330c842036a3
Adriel <adriel_ff94@hotmail.com>
2022-06-17 14:51:29 -0300

    cambio de puerto

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const app = express();
 app.use(express.urlencoded({extended: true}));
 app.use(express.json())
 
-app.listen(3000, () => {
-    console.log(`Listening on port 3000`)
+app.listen(8080, () => {
+    console.log(`Listening on port 8080`)
 });
 
 app.get('/',(req,res) => res.status(200).send('Hello World'))
